ce3fb68bac54c5bd0b48d5f060fc28565830399a
John Papa <johnpapa@users.noreply.github.com>
2018-11-08 15:36:48 +0000

    Server (#1)
    * fixes
    
    * api changes

--- db.js
+++ db.js
@@ -2,46 +2,56 @@ const heroes = [
   {
     id: 'HeroAslaug',
     name: 'Aslaug',
-    description: 'warrior queen'
+    description: 'warrior queen',
   },
   {
     id: 'HeroBjorn',
     name: 'Bjorn Ironside',
-    description: 'king of 9th century Sweden'
+    description: 'king of 9th century Sweden',
   },
   {
     id: 'HeroIvar',
     name: 'Ivar the Boneless',
-    description: 'commander of the Great Heathen Army'
+    description: 'commander of the Great Heathen Army',
   },
   {
     id: 'HeroLagertha',
     name: 'Lagertha the Shieldmaiden',
-    description: 'aka Hlaðgerðr'
+    description: 'aka Hlaðgerðr',
   },
   {
     id: 'HeroRagnar',
     name: 'Ragnar Lothbrok',
-    description: 'aka Ragnar Sigurdsson'
+    description: 'aka Ragnar Sigurdsson',
   },
   {
     id: 'HeroThora',
     name: 'Thora Town-hart',
-    description: 'daughter of Earl Herrauðr of Götaland'
-  }
+    description: 'daughter of Earl Herrauðr of Götaland',
+  },
 ];
 
 const villains = [
   {
     id: 'VillainMadelyn',
     name: 'Madelyn',
-    description: 'chief of theatre props'
+    description: 'chief of theatre props',
   },
   {
     id: 'VillainHaley',
     name: 'Haley',
-    description: 'pen wielder'
-  }
+    description: 'pen wielder',
+  },
+  {
+    id: 'VillainElla',
+    name: 'Ella',
+    description: 'fashionista',
+  },
+  {
+    id: 'VillainLandon',
+    name: 'Landon',
+    description: 'Mandalorian mauler',
+  },
 ];
 
 const data = { heroes: heroes, villains: villains };
