1a32dc90e85fb40bb77258e8f01012ee3d3d3250
John Papa <john@johnpapa.net>
2018-11-15 18:31:31 -0500

    changed the anchor tag to be a button, and styled it

--- src/app/core/header-bar-brand.component.ts
+++ src/app/core/header-bar-brand.component.ts
@@ -4,19 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-header-bar-brand',
   template: `
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://angular.io/" target="_blank" rel="noopener noreferrer">
+      <a
+        class="navbar-item"
+        href="https://angular.io/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i class="fab js-logo fa-angular fa-2x" aria-hidden="true"></i>
       </a>
       <a class="navbar-item nav-home" router-link="/">
-        <span class="tour">TOUR</span>
-        <span class="of">OF</span>
+        <span class="tour">TOUR</span> <span class="of">OF</span>
         <span class="heroes">HEROES</span>
       </a>
-      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <button
+        class="link navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span> <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
+      </button>
     </div>
   `
 })
